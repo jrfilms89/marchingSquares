c30e879c535ad7b6ed670cce451dc58623e8438d
Jon <jrfilms89@gmail.com>
2020-08-28 20:01:48 -0400

    final commit for now. net should add perlin noise

--- script.js
+++ script.js
@@ -1,84 +1,102 @@
 let field = [];
-let rez = 40;
+let rez = 10;
 let cols, rows;
 
-function setup(){
-    createCanvas(600, 400);
-    cols = 1 + width / rez;
-    rows = 1 + height / rez;
-    field = [
-        [cols, rows]
-    ];
-    for (let i = 0; i < cols; i++){
-        for (let j = 0; j < rows; j++){
-            field[[i, j]] = Math.floor(Math.random() * 2);
-        }
+function setup() {
+  createCanvas(600, 400);
+  cols = 1 + width / rez;
+  rows = 1 + height / rez;
+  field = [[cols, rows]];
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      field[[i, j]] = Math.floor(Math.random() * 2);
     }
-    console.log(field);
+  }
+  //   console.log(field);
 }
 
-const line = (v1, v2) =>{
-    line(v1.x, v1.y, v2.x, v2.y);
+const drawLine = (v1, v2) => {
+  line(v1.x, v1.y, v2.x, v2.y);
+};
+
+function draw() {
+  background(120);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      stroke(field[[i, j]] * 255);
+      strokeWeight(rez * 0.4);
+      point(i * rez, j * rez);
+    }
+  }
+  for (let i = 0; i < cols - 1; i++) {
+    for (let j = 0; j < rows - 1; j++) {
+      let x = i * rez;
+      let y = j * rez;
+      let a = createVector(x + rez * 0.5, y);
+      let b = createVector(x + rez, y + rez * 0.5);
+      let c = createVector(x + rez * 0.5, y + rez);
+      let d = createVector(x, y + rez * 0.5);
+      let state = getState(
+        field[[i, j]],
+        field[[i + 1, j]],
+        field[[i + 1, j + 1]],
+        field[[i, j + 1]]
+      );
+      stroke(255);
+      strokeWeight(1);
+      switch (state) {
+        case 1:
+          drawLine(c, d);
+          break;
+        case 2:
+          drawLine(b, c);
+          break;
+        case 3:
+          drawLine(b, d);
+          break;
+        case 4:
+          drawLine(a, b);
+          break;
+        case 5:
+          drawLine(a, d);
+          drawLine(b, c);
+          break;
+        case 6:
+          drawLine(a, c);
+          break;
+        case 7:
+          drawLine(a, d);
+          break;
+        case 8:
+          drawLine(a, d);
+          break;
+        case 9:
+          drawLine(a, c);
+          break;
+        case 10:
+          drawLine(a, b);
+          drawLine(c, d);
+          break;
+        case 11:
+          drawLine(a, b);
+          break;
+        case 12:
+          drawLine(b, d);
+          break;
+        case 13:
+          drawLine(b, c);
+          break;
+        case 14:
+          drawLine(c, d);
+          break;
+        case 15:
+          break;
+      }
+    }
+  }
 }
 
-function draw(){
-    background(120);
-    for (let i = 0; i < cols; i++){
-        for (let j = 0; j < rows; j++){
-            stroke(field[[i, j]]*255);
-            strokeWeight(rez* .4);
-            point(i*rez, j*rez);
-        }
-    }
-    for (let i = 0; i < cols - 1; i++){
-        for (let j = 0; j < rows - 1; j++){
-            let x = i * rez;
-            let y = j * rez;
-            let a = new p5.Vector(x + rez*0.5, y);
-            let b = new p5.Vector(x + rez    , y + rez*0.5);
-            let c = new p5.Vector(x + rez*0.5, y + rez);
-            let d = new p5.Vector(x          , y + rez*0.5);
-            let state = getState(field[[i,j]], field[[i + 1,j]], field[[i + 1 ,j + 1]], field[[i,j + 1]]);
-            stroke(255);
-            strokeWeight(1);
-            switch (state){
-                case 1:
-                    line(a, b);
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                case 11:
-                    break;
-                case 12:
-                    break;
-                case 13:
-                    break;
-                case 14:
-                    break;
-                case 15:
-                    break;
-            }
-        }
-    }
-    
-}
-
-const getState = ( a, b, c, d) =>{
-    return a* 8 + b * 4 + c * 2 + d * 1;
-}
+const getState = (a, b, c, d) => {
+  let s = a * 8 + b * 4 + c * 2 + d * 1;
+  return Math.round(s);
+};
